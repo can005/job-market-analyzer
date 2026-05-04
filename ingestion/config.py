cc0d6b1588a1f5e0c4f2e4db480cf05678f81aef
Carlos Novo <carlosnovo2@gmail.com>
2026-05-04 17:47:51 -0300

    Create ingestion's config file

---
+++ ingestion/config.py
@@ -0,0 +1,9 @@
+RAW_DATA_DIR = 'data/raw/'
+CLEAN_DATA_DIR = 'data/clean/'
+
+AGGREGATE_CSV = "aggregate_job_postings_US.csv"
+SECTOR_CSV = "job_postings_by_sector_US.csv"
+
+
+AGGREGATE_TABLE_NAME = "job_postings_aggregate"
+SECTOR_TABLE_NAME = "job_postings_by_sector"
