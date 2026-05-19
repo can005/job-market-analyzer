8218cb9e2c728500bbeaf4d7f50f76b7db18742d
Carlos Novo <carlosnovo2@gmail.com>
2026-05-18 23:37:16 -0300

    Add HN Jobs Dag

---
+++ dags/hn_jobs_dag.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from airflow.decorators import dag, task
+
+from ingestion.hn_embed import main as hn_embed_main
+from ingestion.hn_fetch import main as hn_fetch_main
+
+
+@dag(
+    dag_id="hn_pipeline",
+    schedule="0 0 1 * *",
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    max_active_runs=1,
+    tags=["hn", "embeddings"],
+)
+def hn_pipeline():
+
+    @task
+    def fetch() -> None:
+        hn_fetch_main()
+
+    @task
+    def embed() -> None:
+        hn_embed_main()
+
+    fetch() >> embed()
+
+
+hn_pipeline()
